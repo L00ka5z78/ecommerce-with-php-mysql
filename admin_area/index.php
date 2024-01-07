1cf34ac887a0f02a1d6ae25e0b59039aa293bbfe
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-07 19:40:29 +0100

    insert product form layout 07.01.24 19:40

--- admin_area/index.php
+++ admin_area/index.php
@@ -41,7 +41,9 @@
                     <p class="text-light text-center">admin name</p>
                 </div>
                 <div class="button text-center">
-                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">Insert products</a></button>
+                    <button class="my-3 px-1">
+                        <a href="panel/insert_product.php" class="nav-link text-light bg-info my-1">Insert products</a>
+                    </button>
                     <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
                     <button class="my-3 px-1"><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert categories</a></button>
                     <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">View categories</a></button>
