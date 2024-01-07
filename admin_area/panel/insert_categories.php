08f0795dd2ab360347fb9b44c4cb357497e9b16f
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-07 18:04:24 +0100

    insert cat 07.01.24 18:04

--- admin_area/panel/insert_categories.php
+++ admin_area/panel/insert_categories.php
@@ -1,4 +1,24 @@
-<?php include('../includes/connect.php') ?>
+<?php
+include('../includes/connect.php');
+
+if (isset($_POST['insert_cat'])) {
+    $category_title             = $_POST['cat_title'];
+
+    //select data from db
+    $select_query               = "SELECT * FROM  `categories` WHERE category_title = '$category_title'";
+    $result_select              = mysqli_query($con, $select_query);
+    $number                     = mysqli_num_rows($result_select);
+    if ($number > 0) {
+        echo "<script>alert('This category is already present in database')</script>";
+    } else {
+        $insert_query           = "INSERT INTO `categories` (category_title) VALUES ('$category_title')";
+        $result                 = mysqli_query($con, $insert_query);
+        if ($result) {
+            echo "<script>alert('Category has been added successfully')</script>";
+        }
+    }
+}
+?>
 
 <form action="" method="post" class="mb-2">
     <div class="input-group mb-2 w-90">
@@ -7,7 +27,6 @@
     </div>
     <div class="input-group w-10 mb-2 m-auto">
 
-        <!-- <input type="submit" name="insert_cat" class="form-control bg-info" value="Insert Categories"> -->
-        <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
+        <input type="submit" name="insert_cat" class="bg-info p-2 border-0 my-3" value="Insert Categories">
     </div>
 </form>
