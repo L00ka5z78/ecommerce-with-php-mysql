2109866a5c2f25c6273b128b1b120f92e8e39d8e
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-07 10:29:32 +0100

    insert categories added 07.01.24 10:29

--- admin_area/index.php
+++ admin_area/index.php
@@ -7,7 +7,8 @@
     <title>Admin dashboard</title>
     <!-- bootstrap css link  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <!-- font awesome link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/styles.css">
 </head>
 
@@ -40,19 +41,31 @@
                     <p class="text-light text-center">admin name</p>
                 </div>
                 <div class="button text-center">
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert products</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert categories</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View categories</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert brands</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View brands</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All orders</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">List users</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">Insert products</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
+                    <button class="my-3 px-1"><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert categories</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">View categories</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">Insert brands</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">View brands</a></button>
+                    <button class="my-3" px-1><a href="" class="nav-link text-light bg-info my-1">All orders</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
+                    <button class="my-3 px-1"><a href="" class="nav-link text-light bg-info my-1">List users</a></button>
+                    <button class="my-3 px-1 "><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
                 </div>
             </div>
         </div>
+
+        <!-- 4 child -->
+        <div class="container my-5">
+            <?php
+            if (isset($_GET['insert_category'])) {
+                include('insert_categories.php');
+            }
+            ?>
+        </div>
+
+
+
         <div class="bg-info p-3 text-center footer">
             <p>All rights reserved Designed by me 2024</p>
         </div>
---
+++ admin_area/insert_categories.php
@@ -0,0 +1,11 @@
+<form action="" method="post" class="mb-2">
+    <div class="input-group mb-2 w-90">
+        <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" name="cat_title" class="form-control" placeholder="Insert categories" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group w-10 mb-2 m-auto">
+
+        <!-- <input type="submit" name="insert_cat" class="form-control bg-info" value="Insert Categories"> -->
+        <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
+    </div>
+</form>
