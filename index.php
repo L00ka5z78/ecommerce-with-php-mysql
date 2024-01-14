9ce787bb198b18bc671f3f8fad0598db70d14b48
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-14 09:40:54 +0100

    adding separate functions in separate files and including them 14.01.24 09:40

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-include('includes/connect.php')
+include('includes/connect.php');
+include('functions/common_function.php');
 
 ?>
 
@@ -83,36 +84,8 @@ include('includes/connect.php')
                 <div class="row">
                     <!-- fetching products from db -->
                     <?php
-                    $select_query                   = "SELECT * FROM `products` ORDER BY rand() LIMIT 0,9";
-                    $result_query                   = mysqli_query($con, $select_query);
-
-                    while ($row = mysqli_fetch_assoc($result_query)) {
-                        $product_id                = $row['product_id'];
-                        $product_title             = $row['product_title'];
-                        $product_description       = $row['product_description'];
-                        $product_image1            = $row['product_image1'];
-                        $product_price             = $row['product_price'];
-                        $category_id               = $row['category_id'];
-                        $brand_id                  = $row['brand_id'];
-                        echo "<div class='col-md-4  mb-2'>
-                        <div class='card'>
-                            <img src='images/$product_image1' class='card-img-top' alt='$product_title'>
-                            <div class='card-body'>
-                                <h5 class='card-title'>$product_title</h5>
-                                <p class='card-text'>$product_description</p>
-                                <a href='#' class='btn btn-info'>Add to cart</a>
-                                <a href='#' class='btn btn-secondary'>View more</a>
-                            </div>
-                        </div>
-                    </div>";
-                    }
+                    getproducts();
                     ?>
-
-
-
-
-
-
                 </div>
             </div>
 
@@ -126,20 +99,9 @@ include('includes/connect.php')
                             <h4>Delivery brands</h4>
                         </a>
                     </li>
-
                     <?php
-                    $select_brands              = "SELECT * FROM `brands`";
-                    $result_brands              = mysqli_query($con, $select_brands);
-                    while ($row_data            = mysqli_fetch_assoc($result_brands)) {
-                        $brand_title            = $row_data['brand_title'];
-                        $brand_id               = $row_data['brand_id'];
-                        echo "<li class='nav-item'>
-                        <a href='index.php?brand=$brand_id' class='nav-link text-light'>$brand_title</a>
-                    </li>";
-                    }
+                    getbrands();
                     ?>
-
-
                 </ul>
 
                 <!-- categories -->
@@ -151,17 +113,7 @@ include('includes/connect.php')
                     </li>
 
                     <?php
-                    $select_categories              = "SELECT * FROM `categories`";
-                    $result_categories              = mysqli_query($con, $select_categories);
-                    while ($row_data                = mysqli_fetch_assoc($result_categories)) {
-                        $category_title            = $row_data['category_title'];
-                        $category_id               = $row_data['category_id'];
-                        echo "<li class='nav-item'>
-                        <a href='index.php?category=$category_id' class='nav-link text-light'>
-                            $category_title
-                        </a>
-                    </li>";
-                    }
+                    getcategories();
                     ?>
                 </ul>
             </div>
