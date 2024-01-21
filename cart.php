399fc6a198bce4f438ecb934bf9ba3384f0e638b
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-21 18:42:12 +0100

    update quantity. ISSUE WITH MULTIPLE PRODUCTS UPDATE 21.01.24 18:42

--- cart.php
+++ cart.php
@@ -16,7 +16,13 @@ include('functions/common_function.php');
     <!-- font awesome link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./styles/styles.css">
-
+    <style>
+        .cart_img {
+            width: 80px;
+            height: 80px;
+            object-fit: contain;
+        }
+    </style>
 </head>
 
 <body>
@@ -79,41 +85,107 @@ include('functions/common_function.php');
         <!-- fourth child table-->
         <div class="container">
             <div class="row">
-                <table class="table table-bordered text-center">
-                    <thead>
-                        <tr>
-                            <th>Product Title</th>
-                            <th>Product Image</th>
-                            <th>Quantity </th>
-                            <th>Total Price</th>
-                            <th>Remove</th>
-                            <th>Operations</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Apple</td>
-                            <td><img class="img-fluid img-thumbnail" style="height: 100px;" src="./images/apple1.jpg" alt="123"></td>
-                            <td><input type="text"></td>
-                            <td>9000</td>
-                            <td><input type="checkbox"></td>
-                            <td>
-                                <p>Update</p>
-                                <p>Remove</p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <!-- subtotal  -->
-                <div class="d-flex mb-5">
-                    <h4 class="px-3">Subtotal: <strong class="text-info">5000/-</strong></h4>
-                    <a href="index.php"><button class="bg-info px-3 py-2 mx-3 border-0">Continue Shopping</button></a>
-                    <a href="#12"><button class="bg-secondary px-3 py-2 border-0 text-light">Checkout</button></a>
-                </div>
+                <form action="" method="post">
+                    <table class="table table-bordered text-center">
+                        <thead>
+                            <tr>
+                                <th>Product Title</th>
+                                <th>Product Image</th>
+                                <th>Quantity </th>
+                                <th>Total Price</th>
+                                <th>Remove</th>
+                                <th colspan="2">Operations</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- php to display dynamic data  -->
+                            <?php
+                            // global $con;
+                            $get_ip_add                         = getIPAddress();
+                            $total_price                        = 0;
+                            $cart_query                         = "
+                            SELECT 
+                            * 
+                            FROM 
+                                `cart_details` 
+                            WHERE 
+                                ip_address = 
+                                '$get_ip_add'
+                            ";
+                            $result                             = mysqli_query($con, $cart_query);
+                            while ($row                         = mysqli_fetch_array($result)) {
+                                $product_id = $row['product_id'];
+                                $select_products                = "
+                            SELECT 
+                            * 
+                            FROM 
+                                `products` 
+                            WHERE 
+                                product_id = 
+                                '$product_id'
+                            ";
+                                $result_product                 = mysqli_query($con, $select_products);
+                                while ($row_product_price       = mysqli_fetch_array($result_product)) {
+                                    $product_price              = array($row_product_price['product_price']);
+
+                                    $price_table                = $row_product_price['product_price'];
+
+                                    $product_title              = $row_product_price['product_title'];
+                                    $product_image1             = $row_product_price['product_image1'];
+
+                                    $product_values             = array_sum($product_price);
+                                    $total_price                += $product_values;
+
+                            ?>
+
+                                    <tr>
+                                        <td><?php echo $product_title ?></td>
+                                        <td><img class=" cart_img" src="./images/<?php echo $product_image1 ?>" alt="123"></td>
+                                        <td><input type="text" class="form-input w-50" name="qty"></td>
+
+                                        <?php
+                                        $get_ip_add                             = getIPAddress();
+                                        if (isset($_POST['update_cart'])) {
+                                            $quantities                         = $_POST['qty'];
+                                            $update_cart                        = "
+                                                UPDATE 
+                                                    `cart_details`
+                                                SET
+                                                    quantity =
+                                                    $quantities  
+                                                WHERE
+                                                    ip_address = '$get_ip_add' 
+                                                ";
+                                            $result_product_quantity            = mysqli_query($con, $update_cart);
+                                            $total_price                        = $total_price * $quantities;
+                                        }
+
+                                        ?>
+                                        <td><?php echo $price_table ?>/-</td>
+                                        <td><input type="checkbox"></td>
+                                        <td>
+                                            <!-- <button class="bg-info px-3 py-2 mx-3 border-0">Update</button> -->
+                                            <input class="bg-info px-3 py-2 mx-3 border-0" type="submit" value="Update Cart" name="update_cart">
+
+                                            <button class="bg-info px-3 py-2 mx-3 border-0">Remove</button>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                    <!-- subtotal  -->
+                    <div class="d-flex mb-5">
+                        <h4 class="px-3">Subtotal: <strong class="text-info"><?php echo $total_price ?>/-</strong></h4>
+                        <a href="index.php"><button class="bg-info px-3 py-2 mx-3 border-0">Continue Shopping</button></a>
+                        <a href="#12"><button class="bg-secondary px-3 py-2 border-0 text-light">Checkout</button></a>
+                    </div>
 
             </div>
         </div>
-
+        </form>
 
         <?php include('./includes/footer.php') ?>
     </div>
