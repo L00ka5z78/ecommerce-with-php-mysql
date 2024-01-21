69e6452ad33ff402bd4ac9410668c23b2058820e
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-21 10:07:53 +0100

    cart function added 21.01.24 10:07

--- functions/common_function.php
+++ functions/common_function.php
@@ -23,7 +23,7 @@ function getproducts()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
             </div>
         </div>
@@ -55,7 +55,7 @@ function get_all_products()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>                
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
             </div>
         </div>
@@ -99,8 +99,8 @@ function get_unique_categories()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>               
+                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
             </div>
         </div>
     </div>";
@@ -142,7 +142,7 @@ function get_unique_brands()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
             </div>
         </div>
@@ -217,7 +217,7 @@ function search_product()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>                
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
             </div>
         </div>
@@ -263,8 +263,8 @@ function view_details()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a> 
+                <a href='index.php' class='btn btn-secondary'>Go home</a>
             </div>
         </div>
     </div>
@@ -309,4 +309,45 @@ function getIPAddress()
     return $ip;
 }
 // $ip = getIPAddress();  
-// echo 'User Real IP Address - '.$ip;  
+// echo 'User Real IP Address - '.$ip; 
+
+//cart function
+
+function cart()
+{
+    if (isset($_GET['add_to_cart'])) {
+        global $con;
+        $get_ip_add                     = getIPAddress();
+        $get_product_id         = $_GET['add_to_cart'];
+        $select_query           = "
+            SELECT
+            *    
+            FROM 
+                `cart_details`
+            WHERE
+                ip_address =
+                '$get_ip_add'
+            AND
+                product_id = 
+                $get_product_id
+            ";
+        $result_query           = mysqli_query($con, $select_query);
+        $num_of_rows            = mysqli_num_rows($result_query);
+        if ($num_of_rows > 0) {
+            echo "<script>alert('This item is already inside cart')</script>";
+            echo "<script>window.open('index.php', '_self')</script>";
+        } else {
+            $insert_query = "
+                INSERT 
+                INTO
+                    `cart_details`
+                    (product_id, ip_address, quantity)
+                VALUES  
+                    ($get_product_id, '$get_ip_add', 0)
+                ";
+            $result_query       = mysqli_query($con, $insert_query);
+            echo "<script>alert('Product added to your cart')</script>";
+            echo "<script>window.open('index.php', '_self')</script>";
+        }
+    }
+}
