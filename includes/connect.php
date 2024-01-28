86f9014dba0133c1e63db51ca27607bf1aab5680
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-28 18:15:40 +0100

    insert user into db added 28.01.24 18:15

--- includes/connect.php
+++ includes/connect.php
@@ -9,3 +9,11 @@ $con = mysqli_connect($localhost, $user, $password, $db_name);
 if (!$con) {
     die(mysqli_connect_error($con));
 }
+
+// for checking connection purposes
+ /* $con = mysqli_connect($localhost, $user, $password, $db_name);
+if ($con) {
+    echo "connected";
+} else {
+    die(mysqli_connect_error($con));
+} */
