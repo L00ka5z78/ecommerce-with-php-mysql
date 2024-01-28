4cd05d795a300b017062a151c5419782b06c38d5
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-01-28 19:44:25 +0100

    user login logic 28.01.24 19:44

--- user_area/user_login.php
+++ user_area/user_login.php
@@ -1,3 +1,8 @@
+<?php
+include('../includes/connect.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,6 +14,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="./styles/styles.css">
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+    </style>
 </head>
 
 <body>
@@ -16,7 +26,7 @@
         <h2 class="text-center">Login form</h2>
         <div class="row d-flex align-items-center justify-content-center mt-5">
             <div class="col-lg-12 col-xl-6">
-                <form action="" method="post" enctype="multipart/form-data">
+                <form action="" method="post">
                     <div class="form-outline mb-4">
                         <label for="user_username" class="form-label">Username</label>
                         <input type="text" id="user_username" class="form-control" placeholder="insert name" autocomplete="off" required="required" name="user_username" />
@@ -38,3 +48,34 @@
 </body>
 
 </html>
+
+<?php
+if (isset($_POST['user_login'])) {
+    $user_username                  = $_POST['user_username'];
+    $user_password                  = $_POST['user_password'];
+
+    $select_query =                 "
+        SELECT 
+        * 
+        FROM 
+            `user_table`
+        WHERE
+            username = 
+            '$user_username'
+        ";
+    $result                         = mysqli_query($con, $select_query);
+    $row_count                      = mysqli_num_rows($result);
+    $row_data                       = mysqli_fetch_assoc($result);
+
+    if ($row_count > 0) {
+        if (password_verify($user_password, $row_data['user_password'])) {
+            echo "<script>alert('You are logged in')</script>";
+        } else {
+            echo "<script>alert('Invalid credentials')</script>";
+        }
+    } else {
+        echo "<script>alert('Invalid credentials')</script>";
+    }
+}
+
+?>
