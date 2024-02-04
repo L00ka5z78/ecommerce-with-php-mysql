5afcf76fdb3a832c50f17cfe981ffcc8a9121261
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-02-04 20:12:50 +0100

    update user feat. form display issue 04.02.24 20:12

--- user_area/edit_account.php
+++ user_area/edit_account.php
@@ -1,3 +1,27 @@
+<?php
+if (isset($_GET['edit_acount'])) {
+    $user_session_name                          = $_SESSION['username'];
+    $select_query                               = "
+        SELECT
+        *
+        FROM
+            `user_table`
+        WHERE
+            username =
+            '$user_session_name'
+        ";
+    $result_query                               = mysqli_query($con, $select_query);
+    $row_fetch                                  = mysqli_fetch_assoc($result_query);
+    $user_id                                    = $row_fetch['user_id'];
+    $username                                   = $row_fetch['username'];
+    $user_email                                 = $row_fetch['user_email'];
+    $user_address                               = $row_fetch['user_address'];
+    $user_mobile                                = $row_fetch['user_mobile'];
+    $user_image = $row_fetch['user_image'];
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,21 +35,22 @@
     <h3 class="text-success mb-4">Edit account</h3>
     <form action="" method="post" enctype="multipart/form-data">
         <div class="form-outline mb-4">
-            <input type="text" class="form-control w-50 m-auto" name="user_username">
+            <input type="text" class="form-control w-50 m-auto" name="user_username" value="<?php echo $username ?>">
         </div>
         <div class="form-outline mb-4">
-            <input type="email" class="form-control w-50 m-auto" name="user_email">
+            <input type="email" class="form-control w-50 m-auto" value="<?php echo $user_email ?>" name="user_email">
         </div>
         <div class="form-outline mb-4 d-flex w-50 m-auto">
             <input type="file" class="form-control m-auto" name="user_image">
             <img src="./user_images/<?php echo $user_image ?>" alt="" class="edit_img">
         </div>
         <div class="form-outline mb-4">
-            <input type="text" class="form-control w-50 m-auto" name="user_address">
+            <input type="text" class="form-control w-50 m-auto" name="user_address" value="<?php echo $user_address ?>">
         </div>
         <div class="form-outline mb-4">
-            <input type="text" class="form-control w-50 m-auto" name="user_mobile">
+            <input type="text" class="form-control w-50 m-auto" name="user_mobile" value="<?php echo $user_mobile ?>">
         </div>
+
         <input type="submit" value="Update" class="bg-info py-2 px-3 border-0" name="user_update">
 
     </form>
