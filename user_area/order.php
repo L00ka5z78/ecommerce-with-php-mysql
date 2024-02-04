6e4c327f8066011bddd7bb99ae02861d4203eb47
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-02-04 10:26:57 +0100

    insert orders into table 04.02.24 10:26

--- user_area/order.php
+++ user_area/order.php
@@ -23,6 +23,10 @@ $cart_query_price                   = "
         '$get_ip_address'    
     ";
 $result_cart_price                  = mysqli_query($con, $cart_query_price);
+$invoice_number                     = mt_rand();    //generates random number
+$status                             = 'pending';
+
+
 $count_products                     = mysqli_num_rows($result_cart_price);
 
 while ($row_price = mysqli_fetch_array($result_cart_price)) {
@@ -42,4 +46,47 @@ while ($row_price = mysqli_fetch_array($result_cart_price)) {
         $product_values             = array_sum($product_price);
         $total_price                += $product_values;
     }
+}
+// getting quantity from cart
+
+$get_cart                           = "
+    SELECT
+    *
+    FROM
+        `cart_details`
+    ";
+$run_cart                           = mysqli_query($con, $get_cart);
+$get_item_quantity                  = mysqli_fetch_array($run_cart);
+$quantity                           = $get_item_quantity['quantity'];
+if ($quantity == 0) {
+    $quantity                       = 1;
+    $subtotal                       = $total_price;
+} else {
+    $quantity                       = $quantity;
+    $subtotal                       = $total_price * $quantity;
+}
+
+$insert_orders                      = "
+    INSERT
+    INTO
+        `user_orders` (
+        user_id,
+        amount_due,
+        invoice_number,
+        total_products,
+        order_date,
+        order_status
+    ) VALUES (
+        $user_id,
+        $subtotal,
+        $invoice_number,
+        $count_products,
+        NOW(),
+        '$status'
+    )";
+
+$result_query                       = mysqli_query($con, $insert_orders);
+if ($result_query) {
+    echo "<script>alert('Orders submitted successfully')</script>";
+    echo "<script>window.open('profile.php', '_self')</script>";
 }
