e2f15f89611ca3a6a4a3ae7a6586ddb3711c9298
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-02-17 19:08:29 +0100

    display user orders 17.02.24 19:08

--- user_area/payment.php
+++ user_area/payment.php
@@ -18,6 +18,14 @@ include('../functions/common_function.php');
     <!-- font awesome link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/styles.css">
+    <style>
+        .payment_image {
+            width: 600px;
+            height: 400px;
+            margin: auto;
+            display: block;
+        }
+    </style>
 </head>
 
 <body>
