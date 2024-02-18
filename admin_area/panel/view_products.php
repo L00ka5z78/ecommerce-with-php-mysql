1b27028fd26ea834dfc283861fd03cdd3f15ea6b
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-02-18 18:34:11 +0100

    admin view added 17.02.24 18:34

---
+++ admin_area/panel/view_products.php
@@ -0,0 +1,27 @@
+<h3 class="text-center text-success">All products</h3>
+<table class="table table-bordered mt-5">
+    <thead>
+        <tr>
+            <th class="bg-info">Product ID</th>
+            <th class="bg-info">Product Title</th>
+            <th class="bg-info">Product Image</th>
+            <th class="bg-info">Product Price</th>
+            <th class="bg-info">Total Sold</th>
+            <th class="bg-info">Status</th>
+            <th class="bg-info">Edit</th>
+            <th class="bg-info">Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="text-center">
+            <td class="bg-secondary text-light">1</td>
+            <td class="bg-secondary text-light">mango</td>
+            <td class="bg-secondary text-light">image</td>
+            <td class="bg-secondary text-light">333</td>
+            <td class="bg-secondary text-light">1</td>
+            <td class="bg-secondary text-light">true</td>
+            <td class="bg-secondary text-light"><a href="" class="text-ligth"><i class="fa-solid fa-pen-to-square text-light"></i></a></td>
+            <td class="bg-secondary text-light"><a href="" class="text-ligth"><i class="fa-solid fa-trash text-light"></i></a></td>
+        </tr>
+    </tbody>
+</table>
