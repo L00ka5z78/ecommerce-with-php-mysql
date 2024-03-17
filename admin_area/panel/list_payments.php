f71db3e76d117db53915382b4cc09510f6ecb985
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-03-17 17:54:25 +0100

    view payments added 17.03.24 17:54

--- admin_area/panel/list_payments.php
+++ admin_area/panel/list_payments.php
@@ -25,23 +25,22 @@
         <tbody>
             ";
             $number = 0;
-            while ($row_data    = mysqli_fetch_assoc($result)) {
-                $order_id       = $row_data['order_id'];
-                $user_id        = $row_data['user_id'];
-                $amount_due     = $row_data['amount_due'];
-                $invoice_number = $row_data['invoice_number'];
-                $total_products = $row_data['total_products'];
-                $order_date     = $row_data['order_date'];
-                $order_status   = $row_data['order_status'];
+            while ($row_data        = mysqli_fetch_assoc($result)) {
+                $order_id           = $row_data['order_id'];
+                $payment_id         = $row_data['payment_id'];
+                $amount             = $row_data['amount'];
+                $invoice_number     = $row_data['invoice_number'];
+                $payment_mode       = $row_data['payment_mode'];
+                $date               = $row_data['date'];
                 $number++;
         ?>
                 <tr>
                     <th class='bg-secondary text-light'><?php echo $number ?></th>
-                    <th class='bg-secondary text-light'><?php echo $amount_due ?></th>
                     <th class='bg-secondary text-light'><?php echo $invoice_number ?></th>
-                    <th class='bg-secondary text-light'><?php echo $total_products ?></th>
-                    <th class='bg-secondary text-light'><?php echo $order_date ?></th>
-                    <th class='bg-secondary text-light'><?php echo $order_status ?></th>
+                    <!-- <th class='bg-secondary text-light'><?php echo $invoice_number ?></th> -->
+                    <th class='bg-secondary text-light'><?php echo $amount ?></th>
+                    <th class='bg-secondary text-light'><?php echo $payment_mode ?></th>
+                    <th class='bg-secondary text-light'><?php echo $date ?></th>
                     <td class='bg-secondary text-light'><a href='index.php?delete_order=<?php echo $order_id ?>' type="button" class="text-light" data-toggle="modal" data-target="#exampleModal"><i class='fa-solid fa-trash text-light'></i></a></td>
                 </tr>
         <?php
