f2b58a9a4139181b0fb97cd09db2e844fcf68d1d
Lukasz Bolach <bravo1_lima1@yahoo.com>
2024-03-23 18:09:29 +0100

    admin login  23.03.24 18:09

--- admin_area/panel/admin_registration.php
+++ admin_area/panel/admin_registration.php
@@ -39,7 +39,7 @@
                     </div>
                     <div>
                         <input type="submit" class="bg-info py-2 px-3 border-0" name="admin_registration" value="Register">
-                        <p class="small fw-bold mt-2 pt-1">Don't you have account?
+                        <p class="small fw-bold mt-2 pt-1">Have account already?
                             <a href="admin_login.php" class="link-danger">Login</a>
                         </p>
                     </div>
